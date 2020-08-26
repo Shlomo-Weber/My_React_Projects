d1e05659a7ad73265cbf0caa7a2ada61acf70f61
Shlomo Weber <shlomo.d.weber@gmail.com>
2020-08-26 14:37:44 +0300

    Started adding funcionality: add expense, filters, and basic formatting of the app

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import {addExpense, removeExpense, editExpense} from './actions/expenses'
@@ -10,12 +11,19 @@ import './styles/styles.scss'
 
 const store = configureStore()
 
-store.dispatch(addExpense({description: 'Water Bill', note: 'pay this now', amount: 350}))
+store.dispatch(addExpense({description: 'Water Bill', note: 'pay this now', amount: 350 }))
+store.dispatch(addExpense({description: 'Gas Bill', note: 'pay this soon', amount: 45, createdAt:1000 }))
 store.dispatch(addExpense({description: 'Electric Bill', note: 'pay this soon', amount: 500}))
-store.dispatch(setTextFilter('bill'))
+
 
 const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-    console.log(visibleExpenses)
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visibleExpenses)
 
-ReactDOM.render(<AppRouter/>, document.getElementById('app'));
+const jsx = (
+    <Provider store = {store}>
+        <AppRouter />
+    </Provider>
+)
+    
+ReactDOM.render(jsx, document.getElementById('app'));
